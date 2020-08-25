444d3eb29f7fa1fb3542c2eb35772c11914c5129
Daniel Gallagher <danielbgallagher@gmail.com>
2020-08-25 19:56:18 -0400

    fixed duplicate movie cards when rendering

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ $(document).ready(function(){
         
         
         const movieHTML = movieArray.map(currentMovie => {
-            $('.movie-container').remove();
-            $(".movie").append(`<div class="card" style="width: 18rem;"><img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">${currentMovie.Title}<span class = "year">${currentMovie.Year}</h5><button type = "button" onclick = "saveToWatchList(${currentMovie.imdbID})" class="btn btn-primary">Add</button> `)
+            
+            $(".movie").append(`<div class="card" style="width: 18rem;"><img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">${currentMovie.Title}
+            <span class = "year">${currentMovie.Year}</h5><button type = "button" onclick = "saveToWatchList(${currentMovie.imdbID})" class="btn btn-primary">Add</button> `)
             
         });
         
@@ -13,7 +14,7 @@ $(document).ready(function(){
     }
 
     document.getElementById('search-form').addEventListener('submit', function(e){
-        
+        $("#movie").empty();
         e.preventDefault();
         console.log(e);
         renderMovies(movieData);
