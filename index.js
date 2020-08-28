359208e22246178824df732da5bedea4d0a96205
Daniel Gallagher <danielbgallagher@gmail.com>
2020-08-27 21:35:46 -0400

    search data base for movies instead of hard coded movie data.js file

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ $(document).ready(function(){
 
     function renderMovies(movieArray){
         
-        
         const movieHTML = movieArray.map(currentMovie => {
             
-            $(".movie").append(`<div class="card" style="width: 18rem;"><img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">${currentMovie.Title}
+            $(".movie").append(`<div class="card" style="width: 18rem;"><a href = "https://www.imdb.com/title/${currentMovie.imdbID}" target = "_blank"><img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap"></a> <div class="card-body"> <h5 class="card-title">${currentMovie.Title}
             <span class = "year">${currentMovie.Year}</h5><button type = "button" onclick = "saveToWatchList('${currentMovie.imdbID}')" class="btn btn-primary">Add</button> `)
             
         });
-        
-        
     }
 
     document.getElementById('search-form').addEventListener('submit', function(e){
         $("#movie").empty();
         e.preventDefault();
-        console.log(e);
-        renderMovies(movieData);
+        let searchString = $(".search-bar")[0].value;
         
+        let urlEncodedSearchString = encodeURIComponent(searchString);
         
+        axios.get(`http://www.omdbapi.com/?apikey=8534d2a7&s=${urlEncodedSearchString}`).then(response =>{
+            renderMovies(response.data.Search);
+        });
     })
 
     
@@ -30,20 +30,27 @@ $(document).ready(function(){
 
 function saveToWatchList(imdbID){
     console.log("saveToWatchList called")
-    var movie = movieData.find(currentMovie => {
-        return currentMovie.imdbID == imdbID
 
+    axios.get(`http://www.omdbapi.com/?apikey=8534d2a7&i=${imdbID}`).then(movie => {
+        var watchlistJSON = localStorage.getItem('watchlist');
+        var watchlist = JSON.parse(watchlistJSON);
+
+        if(watchlist == null){
+            watchlist = [];
+        }
+
+     
+        watchlist.push(movie);
+
+        watchlistJSON = JSON.stringify(watchlist);
+
+        localStorage.setItem('watchlist', watchlistJSON);
     })
-    var watchlistJSON = localStorage.getItem('watchlist');
-    var watchlist = JSON.parse(watchlistJSON);
+    
 
-    if(watchlist == null){
-        watchlist = [];
-    }
 
-    watchlist.push(movie);
 
-    watchlistJSON = JSON.stringify(watchlist);
 
-    localStorage.setItem('watchlist', watchlistJSON);
-}
+
+    
+ }
--- watchlist.js
+++ watchlist.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
     var watchList = localStorage.getItem('watchlist');
 
     var yourWatchList = JSON.parse(watchList);
-    //console.log(yourWatchList);
+    
 
     function renderWatchList(watchList){
         const movieHTML = watchList.map(currentMovie => {
-            //console.log(currentMovie)
-            $(".movie").append(`<div class="card" style="width: 18rem;"><img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">${currentMovie.Title}
-            // <span class = "year">${currentMovie.Year}</h5><button type = "button"  class="btn btn-primary">Remove</button> `)
+            console.log(currentMovie)
+            $(".movie").append(`<div class="card" style="width: 18rem;"><a href = "https://www.imdb.com/title/${currentMovie.data.imdbID}" target = "_blank"><img class="card-img-top" src="${currentMovie.data.Poster}" alt="Card image cap"></a> <div class="card-body"> <h5 class="card-title">${currentMovie.data.Title}
+             <span class = "year">${currentMovie.data.Year}</h5><button type = "button"  class="btn btn-primary">Remove</button> `)
             
         });
     }
